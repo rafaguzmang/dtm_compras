d80d67c7fec5ced74f8762c063d6555c8f4c65b8
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-09-19 08:51:55 -0600

    Se quitan permisos para poder borrar o crear

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,10 @@
     "description": "Modulo para el área de compras",
     "depends":['base', 'mail',"dtm_procesos"],
     "data":[
+        #Security
         'security/ir.model.access.csv',
+        'security/res_groups.xml',
+        'security/model_access.xml',
         #Views
         'views/dtm_compras_requerido_views.xml',
         'views/dtm_compras_realizado_views.xml',
