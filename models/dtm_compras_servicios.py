316c0d3d8902f12b3cd1f320acf27acb45c9a181
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-10-18 12:33:55 -0600

    Se agregan indicadores para los servicios que cuentan con todo el material

--- models/dtm_compras_servicios.py
+++ models/dtm_compras_servicios.py
@@ -16,3 +16,20 @@ class Servicios(models.Model):
     fecha_entrada = fields.Date(string="Fecha de Entrada",readonly=True)
     material_id = fields.Many2many("dtm.materials.line",readonly=True)
     anexos_id = fields.Many2many("ir.attachment",readonly=True)
+    comprado = fields.Char(string="Recibido", readonly=True)
+    listo = fields.Boolean()
+
+    def get_view(self, view_id=None, view_type='form', **options):
+        res = super(Servicios,self).get_view(view_id, view_type,**options)
+
+        servicios = self.env['dtm.compras.servicios'].search([("comprado","!=","Recibido")])
+        for servicio in servicios:
+            # print(servicio.nombre,servicio.nombre)
+            get_recibo = self.env['dtm.compras.realizado'].search([("nombre","ilike",servicio.nombre),("orden_trabajo","ilike",servicio.numero_orden),("comprado","=","Recibido")])
+            get_recibo and servicio.write({'comprado': 'Recibido'})
+            sum = 0
+            for material in servicio.material_id:
+                sum += material.materials_required
+            sum == 0 and servicio.write({'listo':True})
+
+        return res
