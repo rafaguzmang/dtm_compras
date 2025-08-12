b1110f2022e69e8dc484367e2a3df19b401c662e
rafaguzmang <55261233+rafaguzmang@users.noreply.github.com>
2025-08-12 11:32:48 -0600

    Se reparan los precios y se agrega suma de la cantidad total en materiales e ingeniería

--- models/dtm_compras_precios.py
+++ models/dtm_compras_precios.py
@@ -16,6 +16,33 @@ class Precios(models.Model):
     def get_view(self, view_id=None, view_type='form', **options):
         res = super(Precios, self).get_view(view_id, view_type, **options)
 
+        get_codigos = self.env['dtm.materiales'].search([])
+        get_precios_code = self.env['dtm.compras.precios'].search([])
+
+        cont = 0
+        for diferente in get_codigos:
+            get_dif = self.env['dtm.compras.precios'].search([('codigo','=',diferente.id),('nombre','!=',f"{diferente.nombre} {diferente.medida}")])
+            if get_dif:
+                cont += 1
+                print('get_dif',get_dif.codigo)
+        print(cont)
+
+        for precio in get_precios_code:
+            get_codigos = self.env['dtm.materiales'].search(
+                [('id', '=', precio.codigo)], limit=1
+            )
+
+            if get_codigos:
+                nombre_ok = str(get_codigos.nombre).lower() in str(precio.nombre).lower()
+                medida_ok = str(get_codigos.medida).lower() in str(precio.nombre).lower()
+
+                if nombre_ok and medida_ok:
+                    # Ambos están contenidos
+
+                    get_codigos.write({'mostrador': precio.precio, 'mayoreo': precio.mayoreo})
+
+        print('-----------------------------------------------')
+
 
 
 
